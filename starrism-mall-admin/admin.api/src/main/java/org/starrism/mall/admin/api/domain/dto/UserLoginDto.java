ea06aeb4418de594abf1ac9f7fce6f92ca1d923d
hedwing <lime-hedwing@hotmail.com>
2022-08-29 15:36:25 +0800

    参数校验

--- starrism-mall-admin/admin.api/src/main/java/org/starrism/mall/admin/api/domain/dto/UserLoginDto.java
+++ starrism-mall-admin/admin.api/src/main/java/org/starrism/mall/admin/api/domain/dto/UserLoginDto.java
@@ -3,7 +3,10 @@ package org.starrism.mall.admin.api.domain.dto;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 import org.starrism.mall.common.domain.Domainizable;
+
+import javax.validation.constraints.NotEmpty;
 
 /**
  * <p>用户登录传参</p>
@@ -16,9 +19,12 @@ import org.starrism.mall.common.domain.Domainizable;
 public class UserLoginDto implements Domainizable {
     private static final long serialVersionUID = -1115450031216939160L;
 
+    @NotEmpty(message = "登录用户名不能为空")
     @ApiModelProperty(value = "登录用户名")
     private String username;
 
+    @NotEmpty(message = "登录密码不能为空")
+    @Length(min = 1, max = 16, message = "登录密码不能超过16位")
     @ApiModelProperty(value = "登录密码")
     private String password;
 }
--- starrism-mall-auth-center/auth.core/src/main/java/org/starrism/mall/auth/core/controller/AuthController.java
+++ starrism-mall-auth-center/auth.core/src/main/java/org/starrism/mall/auth/core/controller/AuthController.java
@@ -3,7 +3,9 @@ package org.starrism.mall.auth.core.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.starrism.mall.admin.api.domain.dto.UserLoginDto;
 import org.starrism.mall.auth.core.domain.vo.AuthInfoVo;
@@ -18,6 +20,7 @@ import org.starrism.mall.common.rest.CommonResult;
  **/
 @Api(value = "认证管理Web", tags = "认证管理接口")
 @RestController
+@Validated
 public class AuthController {
     private AuthService authService;
 
@@ -28,7 +31,7 @@ public class AuthController {
 
     @ApiOperation(value = "用户登录", notes = "用户登录参数")
     @PostMapping(value = "/login")
-    public CommonResult<AuthInfoVo> login(UserLoginDto dto) {
+    public CommonResult<AuthInfoVo> login(@Validated @RequestBody UserLoginDto dto) {
         return CommonResult.success(authService.login(dto));
     }
 }

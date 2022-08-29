d2bb39e40f67c1af0d3546f345e5e9ed6326e1b9
hedwing <lime-hedwing@hotmail.com>
2022-08-29 15:36:37 +0800

    角色模块添加

---
+++ starrism-mall-admin/admin.core/src/main/java/org/starrism/mall/admin/core/domain/entity/BmsRole.java
@@ -0,0 +1,51 @@
+package org.starrism.mall.admin.core.domain.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.starrism.mall.data.domain.entity.AbstractDataEntityTree;
+
+/**
+ * <p>系统角色表</p>
+ *
+ * @author hedwing
+ * @since 2022/8/29
+ */
+@Setter
+@Getter
+@ToString
+@TableName(value = "bms_role")
+public class BmsRole extends AbstractDataEntityTree<BmsRole> {
+    private static final long serialVersionUID = -1450833595279126423L;
+
+    /**
+     * 角色标识
+     */
+    private String roleCode;
+
+    /**
+     * 角色名称
+     */
+    private String roleName;
+
+    /**
+     * 角色描述
+     */
+    private String description;
+
+    /**
+     * 排序
+     */
+    private Integer sort;
+
+    /**
+     * 层级路径
+     */
+    private String hierarchicalPath;
+
+    @Override
+    public BmsRole clone() throws CloneNotSupportedException {
+        return (BmsRole) super.clone();
+    }
+}
---
+++ starrism-mall-admin/admin.core/src/main/java/org/starrism/mall/admin/core/mapper/BmsRoleMapper.java
@@ -0,0 +1,18 @@
+package org.starrism.mall.admin.core.mapper;
+
+import org.starrism.mall.admin.core.domain.entity.BmsRole;
+import org.starrism.mall.data.mapper.CoreMapper;
+
+/**
+ * <p>针对表【bms_role(系统角色表)】的数据库操作Mapper</p>
+ *
+ * @author hedwing
+ * @since 2022-08-29 15:31:50
+ **/
+public interface BmsRoleMapper extends CoreMapper<BmsRole> {
+
+}
+
+
+
+
